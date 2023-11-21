81cb6cebe514c4c0c7aa095588a679ea598ec401
Chris Russell <cgrussell@gmail.com>
2023-11-21 22:35:23 +0000

    Resource categories

--- SpaceTrading.Production/General/Resources/ResourceCategory.cs
+++ SpaceTrading.Production/General/Resources/ResourceCategory.cs
@@ -2,5 +2,14 @@
 {
     public enum ResourceCategory
     {
+        Agricultural = 0,
+        Energy = 1,
+        Food = 2,
+        Mineral = 3,
+        Metals = 5,
+        Industrial = 6,
+        Technology = 7,
+        Weapons = 8,
+        Naval = 9
     }
 }
